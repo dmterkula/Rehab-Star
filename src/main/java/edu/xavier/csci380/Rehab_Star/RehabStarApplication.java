289baac6c40cf07ec9300404cf649eccbadb6252
Eoin Halligan <ehalligan12@gmail.com>
2017-10-03 15:19:15 -0400

    Initial Commit
    Established the project structure using Maven.

---
+++ src/main/java/edu/xavier/csci380/Rehab_Star/RehabStarApplication.java
@@ -0,0 +1,12 @@
+package edu.xavier.csci380.Rehab_Star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehabStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehabStarApplication.class, args);
+	}
+}
