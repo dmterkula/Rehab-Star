70d0f02be07d1e7a74c1232a91e15704974f259a
Lord Taylor Farmer <farmert@xavier.edu>
2017-11-07 15:32:06 -0500

    Fixed Email Server
    Can now run the email server from command line.  It would look something like :  python EmailServer.py PutEmailAddressHere PutPasswordHere

--- src/main/resources/EmailServer.py
+++ src/main/resources/EmailServer.py
@@ -1,44 +1,39 @@
 import smtplib
+import sys
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
-#from py4j.java_gateway import JavaGateway
 
-#gateway = JavaGateway()                   # connect to the JVM
-#random = gateway.jvm.java.util.Random()   # create a java.util.Random instance
-#number1 = random.nextInt(10)              # call the Random.nextInt method
-#number2 = random.nextInt(10)
-#print(number1,number2)
-#addition_app = gateway.entry_point        # get the AdditionApplication instance
-#addition_app.addition(number1,number2)    # call the addition method
+first_arg = sys.argv[1]
+second_arg = sys.argv[2]
 
-#To and From addresses
+    #To and From addresses
 fromaddr = "RehabStarProject@gmail.com"
-toaddr = "RehabStarProject@gmail.com"
+toaddr = first_arg
 
-#Making the msg object that was imported using MIME.
+    #Making the msg object that was imported using MIME.
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
 
-#Email Subject line
+    #Email Subject line
 msg['Subject'] = "Forgot Password"
 
-#Body of email.
-body = "Your password is: " + "h73HDJ2f"
+    #Body of email.
+body = "Your password is: " + second_arg
 msg.attach(MIMEText(body, 'plain'))
 
-#Start connection to gmail.
+    #Start connection to gmail.
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 
-#Login to gmail.
+    #Login to gmail.
 server.login(fromaddr, "X@vier19")
 
-#Change the msg objet to something gmail can understand, i.e. a string.
+    #Change the msg objet to something gmail can understand, i.e. a string.
 text = msg.as_string()
 
-#Send email.
+    #Send email.
 server.sendmail(fromaddr, toaddr, text)
 
-#Close connection with gmail.
+    #Close connection with gmail.
 server.quit()
